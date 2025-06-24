e79713be03f7ed019b98579633120aec2e63173a
Vasil Horodetskyi <Vasil.Horodetskyi@rational-soft.com>
2025-06-24 12:49:35 +0100

    Initial Commit

---
+++ RadioCarTests/RadioCarTests.swift
@@ -0,0 +1,17 @@
+//
+//  RadioCarTests.swift
+//  RadioCarTests
+//
+//  Created by Maestro Master on 24/06/2025.
+//
+
+import Testing
+@testable import RadioCar
+
+struct RadioCarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
